81cc82273cba21ab7e2dfbe87d07c8246125cfe9
Prayag C Bose <prayagcbose@gmail.com>
2020-07-20 20:37:48 +0530

    index page visual aspect completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,25 +5,47 @@ import janamPic from "./janam.jpg";
 
 function Footer() {
 	return (
-		<Container fluid className="footer-bg">
-			<Row>
-				<Row className="d-flex justify-content-center w-100">
-					<Col md={6}>
-						<h6>Jannamunnani</h6>
-						<p>FOR the people country's most vibrant news channesl</p>
-					</Col>
-					<Col md={6} className="d-flex justify-content-center">
-						<span>
-							Address: <br></br>COLLEGE OF ENGINEERING <br></br> ,THALASSERY
-							<br></br>
-							CETTHALASSERY@gmail.com <br></br>Phone:7356725339 <br></br>
-							9497600974
-						</span>
-					</Col>
+		<div className="f-bg">
+			<Container>
+				<Row className="f-row-main">
+					<Row className="f-row-1 w-100">
+						<Col md={6} className="f-section-1 ">
+							<div className="f-text1">
+								<h3>
+									<a href="#top"> Jannamunnani</a>
+								</h3>
+								<p>FOR the people country's</p>
+								<p>most vibrant news channesl</p>
+							</div>
+						</Col>
+						<Col md={6} className="f-section-2">
+							<span>
+								Address: <br></br>COLLEGE OF ENGINEERING <br></br> ,THALASSERY
+								<br></br>
+								CETTHALASSERY@gmail.com <br></br>Phone:7356725339 <br></br>
+								9497600974
+							</span>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={12} className="d-flex justify-content-center">
+							<hr></hr>
+							<div className="w-80">
+								<img src={janamPic} alt="janamunnani" />
+							</div>
+						</Col>
+					</Row>
+					<Row>
+						<Col xs={12} className="d-flex justify-content-center">
+							<p className="f-copy">
+								Copyright © 2020 All rights reserved | Designed and Developed by
+								DOCKFOX
+							</p>
+						</Col>
+					</Row>
 				</Row>
-			</Row>
-			<hr></hr>
-		</Container>
+			</Container>
+		</div>
 	);
 }
 
